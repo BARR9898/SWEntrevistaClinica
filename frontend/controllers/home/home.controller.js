95cd66f5716c53f8ffda9da8bb06726ab57ece89
BARR9898 <isc.rodriguez.brayan@gmail.com>
2023-03-11 14:30:21 -0600

    Primeras modificaciones al proyecto

--- frontend/controllers/home/home.controller.js
+++
@@ -1,4 +0,0 @@
-app.controller('homeController', function($scope) {
-    // create a message to display in our view
-    $scope.message = 'Hello world, this is the home page!';
-});
--- frontend/script.js
+++ frontend/script.js
@@ -2,6 +2,28 @@
 // include ngRoute for all our routing needs
 var app = angular.module('app', ['ngRoute','ngMaterial']);
 
+app.controller('homeController', function($scope,$mdSidenav) {
+
+    //SIDENAV
+    //Comportamiento del sidenav
+    $scope.toggleSidenav = buildToggler('closeEventsDisabled');
+
+    function buildToggler(componentId) {
+        return function() {
+            $mdSidenav(componentId).toggle();
+        }
+    }
+
+    $scope.opciones = [
+        inicio = {description: "Inicio" , icon: "home" , link:"#!/inicio"} ,
+        expedientes = {description: "Expediente" , icon: "description", link:"#!/expedientes" } ,
+        pacientes = {description: "Pacientes" , icon: "person", link:"#!/pacientes" },
+        
+    ]
+
+
+});
+
 // configure our routes
   app.config(function($routeProvider) {
   $routeProvider
@@ -22,6 +44,17 @@ var app = angular.module('app', ['ngRoute','ngMaterial']);
             .when('/expedientes', {
                 templateUrl : 'controllers/expedientes/expedientes.view.html',
                 controller  : 'expedientesController'
+            })
+
+
+            // route to create an expedient
+            .when('/crear-expediente', {
+                templateUrl : 'controllers/expedientes/expediente.creacion.view.html',
+                controller  : 'expedienteCreacionController'
+            })
+
+            .otherwise({
+                redirectTo: '/'
             })
 
     });
