939a5d73fa98f97e0bd0eb13c8a80aaf4d935de4
BARR9898 <isc.rodriguez.brayan@gmail.com>
2023-03-14 22:16:32 -0600

    Se agregaron los campos faltantes a la seccion de datos del paciente

--- frontend/controllers/expedientes/expediente.creacion.js
+++ frontend/controllers/expedientes/expediente.creacion.js
@@ -2,8 +2,28 @@ app.controller('expedienteCreacionController', function($scope) {
    $scope.expediente = {
       nombre: "",
       apellido_paterno: "",
-      apellido_materno: ""
+      apellido_materno: "",
+      edad: "",
+      ocupacion: "",
+      sexo: "",
+      direccion: ""
+
   }
+
+  $scope.sexos = [
+   {
+      descripcion: "Masculino",
+      value: "M"
+   },
+   {
+      descripcion: "Femenino",
+      value: "F"
+   },
+   {
+      descripcion: "Otro",
+      value: "O"
+   }
+  ]
 
   $scope.guardar = function (){
       console.log($scope.expediente);
