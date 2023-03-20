464af4a723387ef7e310ce25d67fda7a12ae117a
BARR9898 <isc.rodriguez.brayan@gmail.com>
2023-03-20 16:37:18 -0600

    Datos del expediente completado, Login completado

--- frontend/controllers/expedientes/expediente.creacion.js
+++ frontend/controllers/expedientes/expediente.creacion.js
@@ -6,7 +6,26 @@ app.controller('expedienteCreacionController', function($scope) {
       edad: "",
       ocupacion: "",
       sexo: "",
-      direccion: ""
+      direccion: "",
+      motivo_consulta: "",
+      circunstancias_aparicion: "",
+      sintomas: [],
+      descripcion_fisica: "",
+      area_laboral: "",
+      area_escolar: "",
+      demanda_tratamiento: "",
+      acontecimientos_significativos:"",
+      familiograma: "",
+      desarrollo_psicosexual: "",
+      mapeo_familiar:"",
+      impresion_diagnostica_familia:"",
+      hipotesis_familiar:"",
+      examen_mental:"",
+      indicaciones_diagnosticas:"",
+      impresiones_diagnosticas_dcm_cie: []
+
+
+
 
   }
 
@@ -30,4 +49,79 @@ app.controller('expedienteCreacionController', function($scope) {
       alert("HOLA")
   }
 
+  $scope.auxSintomas = "";
+
+  $scope.guardarSintoma = () => {
+   $scope.expediente.sintomas.push($scope.auxSintomas);
+   $scope.auxSintomas = '';
+   console.log('SINTOMAS' , $scope.expediente.sintomas);
+  }
+
+
+  $scope.mostrarSintomas = false;
+
+  $scope.eliminarSintoma = (index) => {
+   if ( (index == 0) && ($scope.expediente.sintomas.length == 1)  ) {
+      $scope.expediente.sintomas.pop();
+      
+   }else if( (index == 0) && ($scope.expediente.sintomas.length > 1)){
+      $scope.expediente.sintomas.shift();
+
+   }else{
+      $scope.expediente.sintomas.splice(index,index);
+   }
+  }
+
+  $scope.auxEje = $scope.auxCodigo = $scope.auxDcm = $scope.auxCie = $scope.auxTranstorno = "";
+
+  $scope.guardarImpresionDiagnosticaDcmCie = () => {
+      let auxJsonDiagnostico = {
+         eje: "",
+         codigo: "",
+         dcm:"",
+         cie:"",
+         transtorno:""
+      };
+     auxJsonDiagnostico.eje = $scope.auxEje;
+     auxJsonDiagnostico.codigo = $scope.auxCodigo
+     auxJsonDiagnostico.dcm = $scope.auxDcm;
+     auxJsonDiagnostico.cie = $scope.auxCie;
+     auxJsonDiagnostico.transtorno = $scope.auxTranstorno;
+
+     $scope.expediente.impresiones_diagnosticas_dcm_cie.push(auxJsonDiagnostico);
+      
+     auxJsonDiagnostico = {
+         eje: "",
+         codigo: "",
+         dcm:"",
+         cie:"",
+         transtorno:""
+      };
+
+     $scope.auxEje = $scope.auxCodigo = $scope.auxDcm = $scope.auxCie = $scope.auxTranstorno = "";
+
+      console.log($scope.expediente.impresiones_diagnosticas_dcm_cie);
+   []
+
+
+  }
+
+  $scope.eliminarImpresionDiagnosticaDcmCie = (index) => {
+   if ( (index == 0) && ($scope.expediente.sintomas.length == 1)  ) {
+      $scope.expediente.sintomas.pop();
+      
+   }else if( (index == 0) && ($scope.expediente.sintomas.length > 1)){
+      $scope.expediente.sintomas.shift();
+
+   }else{
+      $scope.expediente.sintomas.splice(index,index);
+   }
+  }
+
+  $scope.mostrarDiagnosticoDcmCie = false;
+
+
+
+
+  
 });
--- frontend/script.js
+++ frontend/script.js
@@ -1,60 +1,66 @@
 
 // include ngRoute for all our routing needs
-var app = angular.module('app', ['ngRoute','ngMaterial','ngMessages']);
+var app = angular.module('app', ['ngRoute', 'ngMaterial', 'ngMessages']);
 
-app.controller('homeController', function($scope,$mdSidenav) {
+app.controller('homeController', function ($scope, $mdSidenav) {
 
     //SIDENAV
     //Comportamiento del sidenav
     $scope.toggleSidenav = buildToggler('closeEventsDisabled');
 
     function buildToggler(componentId) {
-        return function() {
+        return function () {
             $mdSidenav(componentId).toggle();
         }
     }
 
     $scope.opciones = [
-        inicio = {description: "Inicio" , icon: "home" , link:"#!/inicio"} ,
-        expedientes = {description: "Expediente" , icon: "description", link:"#!/expedientes" } ,
-        pacientes = {description: "Pacientes" , icon: "person", link:"#!/pacientes" },
-        
+        inicio = { description: "Inicio", icon: "home", link: "#!/inicio" },
+        expedientes = { description: "Expediente", icon: "description", link: "#!/expedientes" },
+        pacientes = { description: "Pacientes", icon: "person", link: "#!/pacientes" },
+
     ]
 
 
 });
 
 // configure our routes
-  app.config(function($routeProvider) {
-  $routeProvider
+app.config(function ($routeProvider) {
+    $routeProvider
 
-            // route for the home page
-            .when('/inicio', {
-                templateUrl : 'controllers/home/home.view.html',
-                controller  : 'homeController'
-            })
+        // route for the home page
+        .when('/inicio', {
+            templateUrl: 'controllers/home/home.view.html',
+            controller: 'homeController'
+        })
 
-            // route for the pacients page
-            .when('/pacientes', {
-                templateUrl : 'controllers/pacientes/pacientes.view.html',
-                controller  : 'pacientesController'
-            })
+        // route for the pacients page
+        .when('/pacientes', {
+            templateUrl: 'controllers/pacientes/pacientes.view.html',
+            controller: 'pacientesController'
+        })
 
-            // route for the expedients page
-            .when('/expedientes', {
-                templateUrl : 'controllers/expedientes/expedientes.view.html',
-                controller  : 'expedientesController'
-            })
+        // route for the expedients page
+        .when('/expedientes', {
+            templateUrl: 'controllers/expedientes/expedientes.view.html',
+            controller: 'expedientesController'
+        })
 
 
-            // route to create an expedient
-            .when('/crear-expediente', {
-                templateUrl : 'controllers/expedientes/expediente.creacion.view.html',
-                controller  : 'expedienteCreacionController'
-            })
+        // route to create an expedient
+        .when('/crear-expediente', {
+            templateUrl: 'controllers/expedientes/expediente.creacion.view.html',
+            controller: 'expedienteCreacionController'
+        })
 
-            .otherwise({
-                redirectTo: '/'
-            })
+        // route to create an expedient
+        .when('/login', {
+            templateUrl: 'controllers/login/login.view.html',
+            controller: 'loginController'
+        })
 
-    });
+        .otherwise({
+            redirectTo: '/'
+        })
+
+});
